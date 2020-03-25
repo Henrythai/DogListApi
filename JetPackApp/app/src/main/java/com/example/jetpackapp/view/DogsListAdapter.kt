c4816a6d03211e28c4e1f6086eadbd70d72dc5fc
Henry Thai <trungthaithanh170496@gmail.com>
2020-03-26 12:01:42 +1300

    commit all file from local

---
+++ JetPackApp/app/src/main/java/com/example/jetpackapp/view/DogsListAdapter.kt
@@ -0,0 +1,11 @@
+package com.example.jetpackapp.view
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class DogsListAdapter {
+
+    class DogViewHolder(v:View) : RecyclerView.ViewHolder(v){
+        
+    }
+}
